77336ddec69d73f7af3037014c235924e4ead0c7
Franco Timpanaro <88697870+FrancoSantiagoTimpanaro@users.noreply.github.com>
2022-03-14 11:58:41 -0300

    Added Js SoyDalto

---
+++ JS SoyDalto/codigo.js
@@ -0,0 +1,10 @@
+saludo = "Hola";
+usuario = " Franco";
+pregunta = "¿Como estas?";
+frase = saludo + usuario + pregunta;
+document.write(frase);
+
+nombre = "Franco Timpanaro"
+frase2 = ` Bienvenido ${nombre} que bueno verte de nuevo`;
+//`alt96`//
+document.write(frase2);
